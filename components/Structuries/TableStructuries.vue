da13e40829782c1d7e6baba55e75ec1ed56520a3
Amenov <amenov.abdulsalam@gmail.com>
2020-12-16 15:29:39 +0600

    AM 1529 16122020

---
+++ components/Structuries/TableStructuries.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="table-structuries">
+    <div class="table-structuries-header">
+      <div>Название</div>
+      <div>Общее кол-во</div>
+      <div>Фактическое кол-во</div>
+      <div>Кол-во вложенных элементов</div>
+    </div>
+    <Structuries :structuries="structuries" />
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+
+export default {
+  computed: {
+    ...mapGetters({
+      structuries: "structuries/structuries"
+    })
+  }
+};
+</script>
+
+<style lang="scss">
+.table-structuries {
+  &-header {
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+
+    > div {
+      padding: 15px 0;
+    }
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,38 +3,8 @@
     <div class="wrapper">
       <Header />
       <div class="content">
-        <div class="structuries-header">
-          <div>Название</div>
-          <div>Общее кол-во</div>
-          <div>Фактическое кол-во</div>
-          <div>Кол-во вложенных элементов</div>
-        </div>
-        <Structuries :structuries="structuries" />
+        <TableStructuries />
       </div>
-      <div class="footer"></div>
     </div>
   </div>
 </template>
-
-<script>
-import { mapGetters, mapActions } from "vuex";
-
-export default {
-  computed: {
-    ...mapGetters({
-      structuries: "structuries/structuries"
-    })
-  }
-};
-</script>
-
-<style lang="scss">
-.structuries-header {
-  display: grid;
-  grid-template-columns: repeat(5, 1fr);
-
-  > div {
-    padding: 15px 0;
-  }
-}
-</style>
